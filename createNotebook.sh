fe26a5e5331fba9e13976cb418e528170bccaf94
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-06-07 18:04:01 +1000

    rename
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- createNotebook.sh
+++ createNotebook.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export SERVER_FULLNAME="${1:-dev.db.faims.edu.au}" # dev, alpha, testing, selenium
-cp csirogeochem.json faims3-temp-notebook-migrator/
+cp burial.json faims3-temp-notebook-migrator/
 cd faims3-temp-notebook-migrator
 echo "Load Burial ${SERVER_FULLNAME}"
 #sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
